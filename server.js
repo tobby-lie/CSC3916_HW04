5b64102bd286a750f31abaca3462c1bc3618d768
Tobby Lie <tobbylie@gmail.com>
2021-03-16 20:52:41 -0600

    updated /movies post route

--- server.js
+++ server.js
@@ -142,10 +142,6 @@ router.route('/movies/:movie_title')
                         })
 
                 }
-                trackDimension(movie.genre, '/movies/:movie_title', 'GET Movie', '1', movie.title, "1")
-                    .then(function(response) {
-                        console.log(response.body);
-                    })
             })
         } else {
             Movie.find({title: req.params.movie_title}).select("title year_released genre actors").exec(function (err, movie) {
@@ -323,6 +319,15 @@ router.route('/reviews')
 router.all('/', function (req, res) {
     return res.status(403).json({ success: false, msg: 'This route is not supported.' });
 });
+
+router.route('/test')
+    .get(function(req, res) {
+        trackDimension('Feedback', 'Rating', 'Feedback for Movie', '3', 'Guardians', '1')
+            .then(function(response) {
+                console.log(response.body);
+                res.status(200).send('Event tracked.').end();
+            });
+    })
 
 app.use('/', router);
 app.listen(process.env.PORT || 8080);

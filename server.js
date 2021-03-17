8290edf9723d45dbcc28fc53a062520a22e119af
Tobby Lie <tobbylie@gmail.com>
2021-03-16 22:29:09 -0600

    updated /movies post route

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ router.route('/movies/:movie_title')
                             if (err) {
                                 return res.status(403).json({success: false, message: "The movie title parameter was not found."});
                             } else {
-                                // trackDimension(movie.genre, '/movies/:movie_title?reviews=true', 'GET Movie', "1", movie.title, "1");
+                                trackDimension(movie.genre, '/movies/:movie_title?reviews=true', 'GET Movie', "1", movie.title, "1");
 
                                 return res.status(200).json({success: true, message: "Movie title passed in and it's reviews were found.", movie: mov});
                             }

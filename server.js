8ae69d58dc898f1bead83295d07e3025ddd8d3a3
Tobby Lie <tobbylie@gmail.com>
2021-03-16 20:17:34 -0600

    updated /movies post route

--- server.js
+++ server.js
@@ -20,6 +20,40 @@ app.use(passport.initialize());
 
 var router = express.Router();
 
+const GA_TRACKING_ID = process.env.GA_KEY;
+
+function trackDimension(category, action, label, value, dimension, metric) {
+    var options = {method: 'GET',
+        url: 'https://www.google-analytics.com/collect',
+        qs:
+            {
+                // API Version.
+                v: '1',
+                // Tracking ID / Property ID.
+                tid: GA_TRACKING_ID,
+                // Random Client Identifier. Ideally, this should be a UUID that
+                // is associated with particular user, device, or browser instance.
+                cid: crypto.randomBytes(16).toString("hex"),
+                // Event hit type.
+                t: 'event',
+                // Event category,
+                ec: category,
+                // Event action.
+                ea: action,
+                // Event label.
+                el: label,
+                // Event value.
+                ev: value,
+                // Custom Dimension
+                cd1: dimension,
+                // Custom Metric
+                cm1: metric
+            },
+        headers:
+            { 'Cache-Control': 'no-cache' } };
+    return rp(options);
+}
+
 function getJSONObjectForMovieRequirement(req) {
     var json = {
         headers: "No headers",
@@ -107,6 +141,24 @@ router.route('/movies/:movie_title')
                             }
                         })
 
+                }
+            })
+        } else {
+            Movie.find({title: req.params.movie_title}).select("title year_released genre actors").exec(function (err, movie) {
+                if (err) {
+                    return res.status(403).json({success: false, message: "Unable to retrieve title passed in."});
+                }
+                if (movie && movie.length > 0) {
+                    return res.status(200).json({
+                        success: true,
+                        message: "Successfully retrieved movie.",
+                        movie: movie
+                    });
+                } else {
+                    return res.status(404).json({
+                        success: false,
+                        message: "Unable to retrieve a match for title passed in."
+                    });
                 }
             })
         }
